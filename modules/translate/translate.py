410b4099519e0ebff0e7657a4e6cd487bd50d66a
roman <roman@kali>
2025-05-21 09:29:33 -0500

    full elements translate

--- modules/translate/translate.py
+++ modules/translate/translate.py
@@ -1,22 +1,14 @@
 import os
 import base64
 
-# Получаем путь до папки bhptrojan
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+dirname = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")) #absolute path
 
-# Вход и выход
-input_dir = os.path.join(BASE_DIR, "data", "abc")
-output_dir = os.path.join(BASE_DIR, "decoded_files")
+input_dir = os.path.join(dirname, "data", "abc")
+output_dir = os.path.join(dirname, "decoded_files")
 
-# Создать выходную папку, если не существует
-os.makedirs(output_dir, exist_ok=True)
-
-for filename in os.listdir(input_dir):
+for filename in os.listdir(input_dir):  #full path 
     input_path = os.path.join(input_dir, filename)
     output_path = os.path.join(output_dir, filename)
-
-    if not os.path.isfile(input_path):
-        continue
 
     try:
         with open(input_path, "r") as f:
@@ -25,8 +17,15 @@ for filename in os.listdir(input_dir):
 
         with open(output_path, "w") as f:
             f.write(decoded)
-
-        print(f"[+] Декодирован: {filename}")
+            print(f"+Yes {filename}")
 
     except Exception as e:
-        print(f"[!] Ошибка в файле {filename}: {e}")
+        print("Mistake %s" % e)
+
+    
+
+
+
+
+
+
